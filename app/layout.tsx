045e9d162acd1d3e6d57a910232f3409a575c717
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2025-12-11 15:06:28 -0800

    Revert "added components"
    This reverts commit efb016cb0a0121a4f7b83ff1fd2522824742c68e.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import {Roboto} from 'next/font/google'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import Footer from '../components/Footer'
-import Header from '../components/Header'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -17,13 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${roboto.variable} font-sans`}>
-      <body className='bg-bg font-mono antialiased selection:bg-accent selection:text-black min-h-screen flex flex-col '>
-        <Header />
-        <main>{children}</main>
-
-        <Footer />
-        <Analytics /> <SpeedInsights />
-      </body>
+      <body className='bg-bg p-16 '>{children} <Analytics /> <SpeedInsights /></body>
     </html>
   )
 }
