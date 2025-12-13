880edcb7c4e5fa785eaa51bdbdcdcc5be069367d
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2025-12-12 20:37:24 -0800

    Refactor: Remove unused frontend and backend skill arrays

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ import ContactSection from '../components/ContactSection';
 
 
 export default function Page() {
-    const skillsFrontend = ['JavaScript', 'C#', 'HTML', 'CSS'];
-    const skillsBackend = ['.Net', 'ASP.NET', 'Node.js', 'Express.js', 'SQL Server', 'PostgreSQL', 'MongoDB', 'Azure', 'Google Cloud', 'Firebase', 'SupaBase'];
+
     return (
         <div className='flex-1 w-full max-w-7xl mx-auto px-6 py-12 flex flex-col gap-24'>
             <Hero />
