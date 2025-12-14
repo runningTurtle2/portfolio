44bb4d6992950988ee524607ae63a4add4e601c5
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2025-12-13 19:56:34 -0800

    Base layout and style done

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -40,6 +40,22 @@ function ExperienceSection() {
                         </ul>
                     </div>
                 </div>
+                {/** Three */}
+                <div className='flex flex-col md:flex-row gap-6 border-l border-border pl-6 md:pl-0 md:border-l-0'>
+                    <div className='md:w-1/4 md:text-right md:pr-6 md:border-r border-border  relative'>
+                        <div className='hidden md:block absolute top-1.5 -right-[5px] w-2.5 h-2.5 bg-border rounded-full border border-black'></div>
+                        <span className='font-bold text-md block mb-1'>TiriVelo Inc.</span>
+                        <span className='text-text-muted text-xs uppercase tracking-wider'>June 2025 - October 2025</span>
+                    </div>
+                    <div className='md:w-3/4 pb-8 md:pb-0 '>
+                        <h3 className='text-xl font-bold mb-2'>Software Developer Internship</h3>
+                        <ul className='space-y-2 text-text-muted text-sm list-none'>
+                            <li className='flex items-start gap-2'><span className='text-accent '>{"›"}</span>Nisi laborum fugiat consectetur amet incididunt ea ipsum culpa magna occaecat nostrud et.</li>
+                            <li className='flex items-start gap-2'><span className='text-accent '>{"›"}</span>Nisi laborum fugiat consectetur amet incididunt ea ipsum culpa magna occaecat nostrud et.</li>                            
+                            <li className='flex items-start gap-2'><span className='text-accent '>{"›"}</span>Nisi laborum fugiat consectetur amet incididunt ea ipsum culpa magna occaecat nostrud et.</li>
+                        </ul>
+                    </div>
+                </div>                
             </div>
         </section>
     )
